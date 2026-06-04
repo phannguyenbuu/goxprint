249d0646b84591ef2b688324e0b68396ed21debf
phannguyenbuu <phannguyenbuu@gmail.com>
2026-06-04 07:01:37 +0700

    bump version to 1.3.115 and recompile loader with stable Python 3.12

--- printagent_v2/pack_agent_core.py
+++ printagent_v2/pack_agent_core.py
@@ -19,7 +19,7 @@ def main():
     
     # We will read version from a file, or prompt / default
     # Let's see if there is an existing version we should use, e.g. "1.3.56"
-    version = "1.3.113"
+    version = "1.3.115"
     
     exclude_names = set()
     
